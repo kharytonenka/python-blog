b7518111024b370d5ec1c00e87bd5f4827cc0b9d
Mikita Kharytonenka <mikita.kharytonenka@gmail.com>
2024-03-25 12:48:41 -0700

    Implement app and add system test with mock

---
+++ tests/system/test_app.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from unittest.mock import patch
+from blog import Blog
+import app
+
+
+class AppTest(TestCase):
+    BLOG_TEST_TITLE_1 = "Blog Test Title 1"
+    BLOG_TEST_AUTHOR_1 = "Blog Test Author 1"
+
+    def test_print_blogs(self):
+        blog = Blog(AppTest.BLOG_TEST_TITLE_1, AppTest.BLOG_TEST_AUTHOR_1)
+        app.blogs[AppTest.BLOG_TEST_TITLE_1] = blog
+
+        with patch('builtins.print') as mocked_print:
+            app.print_blogs()
+            mocked_print.assert_called_with(f"-{AppTest.BLOG_TEST_TITLE_1} by {AppTest.BLOG_TEST_AUTHOR_1} (0 posts)")
