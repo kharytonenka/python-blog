a3b5c58d982c0b2c2544a73d767d0a47f3f94628
Mikita Kharytonenka <mikita.kharytonenka@gmail.com>
2024-03-25 13:08:59 -0700

    Add system test for checking user input with mock

--- tests/system/test_app.py
+++ tests/system/test_app.py
@@ -8,6 +8,17 @@ class AppTest(TestCase):
     BLOG_TEST_TITLE_1 = "Blog Test Title 1"
     BLOG_TEST_AUTHOR_1 = "Blog Test Author 1"
 
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_print:
+            app.menu()
+            mocked_print.assert_called_with(app.MENU_PROMPT)
+
+    def test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_print_blogs.assert_called()
+
     def test_print_blogs(self):
         blog = Blog(AppTest.BLOG_TEST_TITLE_1, AppTest.BLOG_TEST_AUTHOR_1)
         app.blogs[AppTest.BLOG_TEST_TITLE_1] = blog
